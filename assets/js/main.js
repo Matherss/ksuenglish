aba1c7d5c33f4724b4a3868b02142d6afcf7fbb5
Matherss <mathers_beats@mail.ru>
2021-12-25 20:01:09 +0300

    25dec

---
+++ assets/js/main.js
@@ -0,0 +1,236 @@
+/*==================== FORM send ====================*/
+
+var elements = document.getElementsByClassName("_tel");
+for (var i = 0; i < elements.length; i++) {
+  new IMask(elements[i], {
+    mask: "+{7}(000)000-00-00"
+  });
+}
+
+/*==================== MODAL window ====================*/
+!(function (e) {
+  "function" != typeof e.matches &&
+    (e.matches =
+      e.msMatchesSelector ||
+      e.mozMatchesSelector ||
+      e.webkitMatchesSelector ||
+      function (e) {
+        for (var t = this, o = (t.document || t.ownerDocument).querySelectorAll(e), n = 0; o[n] && o[n] !== t; ) ++n;
+        return Boolean(o[n]);
+      }),
+    "function" != typeof e.closest &&
+      (e.closest = function (e) {
+        for (var t = this; t && 1 === t.nodeType; ) {
+          if (t.matches(e)) return t;
+          t = t.parentNode;
+        }
+        return null;
+      });
+})(window.Element.prototype);
+
+document.addEventListener("DOMContentLoaded", function () {
+  var request = new XMLHttpRequest();
+  request.open("POST", "/telegram.php", true);
+  request.setRequestHeader("accept", "application/json");
+  var form = document.querySelector("#form");
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    var formData = new FormData(form);
+    request.onreadystatechange = function () {
+      if (request.status === 200) {
+        alert("Сообщение отправлено");
+        form.reset();
+      }
+    };
+    request.send(formData);
+  });
+
+  /* Записываем в переменные массив элементов-кнопок и подложку.
+      Подложке зададим id, чтобы не влиять на другие элементы с классом overlay*/
+  var modalButtons = document.querySelectorAll(".js-open-modal"),
+    overlay = document.querySelector(".js-overlay-modal"),
+    closeButtons = document.querySelectorAll(".js-modal-close");
+
+  /* Перебираем массив кнопок */
+  modalButtons.forEach(function (item) {
+    /* Назначаем каждой кнопке обработчик клика */
+    item.addEventListener("click", function (e) {
+      /* Предотвращаем стандартное действие элемента. Так как кнопку разные
+            люди могут сделать по-разному. Кто-то сделает ссылку, кто-то кнопку.
+            Нужно подстраховаться. */
+      e.preventDefault();
+
+      /* При каждом клике на кнопку мы будем забирать содержимое атрибута data-modal
+            и будем искать модальное окно с таким же атрибутом. */
+      var modalId = this.getAttribute("data-modal"),
+        modalElem = document.querySelector('.modal[data-modal="' + modalId + '"]');
+
+      /* После того как нашли нужное модальное окно, добавим классы
+            подложке и окну чтобы показать их. */
+      modalElem.classList.add("active");
+      overlay.classList.add("active");
+    }); // end click
+  }); // end foreach
+
+  closeButtons.forEach(function (item) {
+    item.addEventListener("click", function (e) {
+      var parentModal = this.closest(".modal");
+
+      parentModal.classList.remove("active");
+      overlay.classList.remove("active");
+    });
+  }); // end foreach
+
+  document.body.addEventListener(
+    "keyup",
+    function (e) {
+      var key = e.keyCode;
+
+      if (key == 27) {
+        document.querySelector(".modal.active").classList.remove("active");
+        document.querySelector(".overlay").classList.remove("active");
+      }
+    },
+    false
+  );
+
+  overlay.addEventListener("click", function () {
+    document.querySelector(".modal.active").classList.remove("active");
+    this.classList.remove("active");
+  });
+}); // end ready
+
+// function telTest(input) {
+//   console.log(input.value);
+//   return !/^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/.test(input.value);
+// }
+
+/*==================== SHOW MENU ====================*/
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId);
+
+  // Validate that variables exist
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      // We add the show-menu class to the div tag with the nav__menu class
+      nav.classList.toggle("show-menu");
+    });
+  }
+};
+showMenu("nav-toggle", "nav-menu");
+
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll(".nav__link");
+
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+  // When we click on each nav__link, we remove the show-menu class
+  navMenu.classList.remove("show-menu");
+}
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll("section[id]");
+
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    const sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document.querySelector(".nav__menu a[href*=" + sectionId + "]").classList.add("active-link");
+    } else {
+      document.querySelector(".nav__menu a[href*=" + sectionId + "]").classList.remove("active-link");
+    }
+  });
+}
+window.addEventListener("scroll", scrollActive);
+
+/*==================== CHANGE BACKGROUND HEADER ====================*/
+function scrollHeader() {
+  const nav = document.getElementById("header");
+  // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+  if (this.scrollY >= 200) nav.classList.add("scroll-header");
+  else nav.classList.remove("scroll-header");
+}
+window.addEventListener("scroll", scrollHeader);
+
+/*==================== SHOW SCROLL TOP ====================*/
+function scrollTop() {
+  const scrollTop = document.getElementById("scroll-top");
+  // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+  if (this.scrollY >= 560) scrollTop.classList.add("show-scroll");
+  else scrollTop.classList.remove("show-scroll");
+}
+window.addEventListener("scroll", scrollTop);
+
+/*==================== DARK LIGHT THEME ====================*/
+const themeButton = document.getElementById("theme-button");
+const darkTheme = "dark-theme";
+const iconTheme = "bx-toggle-right";
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => (document.body.classList.contains(darkTheme) ? "dark" : "light");
+const getCurrentIcon = () => (themeButton.classList.contains(iconTheme) ? "bx-toggle-left" : "bx-toggle-right");
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](darkTheme);
+  themeButton.classList[selectedIcon === "bx-toggle-left" ? "add" : "remove"](iconTheme);
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener("click", () => {
+  // Add or remove the dark / icon theme
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+  // We save the theme and the current icon that the user chose
+  localStorage.setItem("selected-theme", getCurrentTheme());
+  localStorage.setItem("selected-icon", getCurrentIcon());
+});
+
+/*==================== SCROLL REVEAL ANIMATION ====================*/
+const sr = ScrollReveal({
+  distance: "30px",
+  duration: 1800,
+  reset: true
+});
+
+sr.reveal(
+  `.home__data, .home__img, 
+           .certificates__data,
+           .swiper,
+           .footer__content`,
+  {
+    origin: "top",
+    interval: 200
+  }
+);
+
+sr.reveal(`.send__content`, {
+  origin: "left"
+});
+
+sr.reveal(`.about__data, .send__img`, {
+  origin: "right"
+});
+var swiper = new Swiper(".mySwiper", {
+  loop: true,
+  autoplay: { delay: 7000, disableOnInteraction: false },
+  autoHeight: true,
+  grabCursor: true,
+  effect: "slide",
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  }
+});
