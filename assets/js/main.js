7a1050576494a28fe9ebb1898d8e6723e9cb501b
Matherss <mathers_beats@mail.ru>
2022-01-08 12:25:06 +0300

    did discounts

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,6 +29,50 @@ for (var i = 0; i < elements.length; i++) {
 })(window.Element.prototype);
 
 document.addEventListener("DOMContentLoaded", function () {
+  const discount = 0.7;
+  var priceWithoutDiscount = document.querySelectorAll(".price-wo-discount");
+  let priceWithDiscount = document.querySelectorAll(".price-num");
+
+  for (i = 0; i < priceWithoutDiscount.length; i++) {
+    let price = priceWithoutDiscount[i].innerText * discount;
+    priceWithDiscount[i].innerText = price;
+  }
+
+  const deadline = new Date(2022, 09, 01);
+  // id таймера
+  let timerId = null;
+  // склонение числительных
+  function declensionNum(num, words) {
+    return words[num % 100 > 4 && num % 100 < 20 ? 2 : [2, 0, 1, 1, 1, 2][num % 10 < 5 ? num % 10 : 5]];
+  }
+  // вычисляем разницу дат и устанавливаем оставшееся времени в качестве содержимого элементов
+  function countdownTimer() {
+    const diff = deadline - new Date();
+    if (diff <= 0) {
+      clearInterval(timerId);
+    }
+    const days = diff > 0 ? Math.floor(diff / 1000 / 60 / 60 / 24) : 0;
+    const hours = diff > 0 ? Math.floor(diff / 1000 / 60 / 60) % 24 : 0;
+    const minutes = diff > 0 ? Math.floor(diff / 1000 / 60) % 60 : 0;
+    const seconds = diff > 0 ? Math.floor(diff / 1000) % 60 : 0;
+    $days.textContent = days < 10 ? "0" + days : days;
+    $hours.textContent = hours < 10 ? "0" + hours : hours;
+    $minutes.textContent = minutes < 10 ? "0" + minutes : minutes;
+    $seconds.textContent = seconds < 10 ? "0" + seconds : seconds;
+    $days.dataset.title = declensionNum(days, ["день", "дня", "дней"]);
+    $hours.dataset.title = declensionNum(hours, ["час", "часа", "часов"]);
+    $minutes.dataset.title = declensionNum(minutes, ["минута", "минуты", "минут"]);
+    $seconds.dataset.title = declensionNum(seconds, ["секунда", "секунды", "секунд"]);
+  }
+  // получаем элементы, содержащие компоненты даты
+  const $days = document.querySelector(".timer__days");
+  const $hours = document.querySelector(".timer__hours");
+  const $minutes = document.querySelector(".timer__minutes");
+  const $seconds = document.querySelector(".timer__seconds");
+  // вызываем функцию countdownTimer
+  countdownTimer();
+  // вызываем функцию countdownTimer каждую секунду
+  timerId = setInterval(countdownTimer, 1000);
   var request = new XMLHttpRequest();
   request.open("POST", "/telegram.php", true);
   request.setRequestHeader("accept", "application/json");
@@ -38,7 +82,7 @@ document.addEventListener("DOMContentLoaded", function () {
     var formData = new FormData(form);
     request.onreadystatechange = function () {
       if (request.status === 200) {
-        alert("Сообщение отправлено");
+        document.querySelector(".send__direction").innerText = "Сообщение отправлено!";
         form.reset();
       }
     };
@@ -144,7 +188,9 @@ function scrollActive() {
     if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
       document.querySelector(".nav__menu a[href*=" + sectionId + "]").classList.add("active-link");
     } else {
-      document.querySelector(".nav__menu a[href*=" + sectionId + "]").classList.remove("active-link");
+      if (document.querySelector(".nav__menu a[href*=" + sectionId + "]").classList.contains("active-link")) {
+        document.querySelector(".nav__menu a[href*=" + sectionId + "]").classList.remove("active-link");
+      }
     }
   });
 }
